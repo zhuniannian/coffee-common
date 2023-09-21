8e0f764537b01b715407a5d4ed27e4023e62a3c0
zhuniannian <1506698691@qq.com>
2023-09-21 16:21:41 +0800

    'init'

---
+++ coffee-parents/src/main/java/com/coffee/parents/ParentsApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.parents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentsApplication.class, args);
+	}
+
+}
